8b14f6c9dd1c9431173923d262c5104152d1b155
jbrenes <jbrenesbar@gmail.com>
2013-10-06 19:19:44 -0200

    arreglos en codigo impresora

--- arduino/plotter/plotter.ino
+++ arduino/plotter/plotter.ino
@@ -1,19 +1,20 @@
 #include <Stepper.h>
-const int stepsPerRevolution = 100; 
-Stepper yStepper(stepsPerRevolution, 3,11,12,13); 
-Stepper xStepper(stepsPerRevolution, 7,8,9,6);
+const int stepsPerRevolution = 4; 
+Stepper yStepper(stepsPerRevolution, 3,11); 
+Stepper xStepper(stepsPerRevolution, 7,8);
 Stepper zStepper(stepsPerRevolution,2,4,5,10);
 String commandAux;
 int xPosition=0;
 int yPosition=0;
 int zPosition=0;
 int speedZ=50;
+String OK="OK";
 void setup() {
   Serial.begin(9600);
   moveToStartupPosition();
-  //yStepper.setSpeed(70);  
-  //xStepper.setSpeed(70);
-  //zStepper.setSpeed(70);
+  yStepper.setSpeed(70);  
+  xStepper.setSpeed(800);
+  zStepper.setSpeed(70);
   Serial.println("Ready\n");
   
 }
@@ -23,12 +24,28 @@ void moveToStartupPosition(){
   
 }
 
-void moveToYPosition(int y){
+void moveXSteps(int x){
+   xStepper.step(x);
+   xPosition=xPosition+x;
+}
 
+void moveYSteps(int y){
+   yStepper.step(y);
+   yPosition=yPosition+y;
+}
+
+void moveToYPosition(int y){
+  int dy=y-yPosition;
+  Serial.println(dy);
+  yStepper.step(dy);
+  yPosition=y;
 }
 
 void moveToXPosition(int x){
-
+   int dx=x-xPosition;
+  Serial.println(dx);
+  xStepper.step(dx);
+  xPosition=x;
 }
 
 void moveToZPosition(int z){
@@ -42,15 +59,57 @@ void loop() {
       commandAux.concat(character);
       if(character=='\n'){
         command=commandAux;
+        commandAux="";
       }
-      Serial.flush();
+    
     }
 
     if (command != "") {
-      Serial.println(command);
-      command="";
+      processCommand(command);
     }
-    //Serial.println("Sali");
+   
    
     
+}
+
+void getCommandArgs(String command, String* args){
+  int index = command.indexOf(' ');
+  int aIndex=0;
+  
+  for(int i=index+1; i<command.length(); i++){
+      char current = command.charAt(i);
+      if(current!=','){
+        args[aIndex].concat(command.charAt(i));
+      }else{
+        aIndex++;
+      }
+  }
+}
+
+String getCommandOperation(String command){
+   int spIndex=command.indexOf(' ');
+   String operation="";
+   for(int i=0;i<spIndex; i++){
+       operation.concat(command.charAt(i));
+   }
+   return operation;
+}
+
+void processCommand(String command){
+ 
+  String operation=getCommandOperation(command);
+  String args[10];
+  getCommandArgs(command,&args[0]);
+  if(operation=="moveto"){
+     int y=args[1].toInt();
+     int x=args[0].toInt();
+     moveToYPosition(y);
+     moveToXPosition(x); 
+  }else if(operation=="move"){
+     int y=args[1].toInt();
+     int x=args[0].toInt();
+     moveXSteps(x);
+     moveYSteps(y); 
+  }
+  Serial.println(OK);
 }
